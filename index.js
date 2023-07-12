8082c9c46ec255b9fc1897aa0127230cfc6c3855
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-07-12 07:01:37 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+    if(ride <= 400){
+      return ('This one is on me!');
+    }
+    else if(ride > 2000 && ride < 2500){
+      return('I will gladly take your thirty bucks.')
+    }
+    else{
+      return('No can do.');
+    }
+    
 }
 
-function ternaryCheckCity(){
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+    if(city=="NYC"){
+      return('Ok, sounds good.');
+    }
+    else{
+      return('No go.');
+    }
+
+  }
+
+
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+    if(tip=='generous'){
+      return("Thank you so much.")
+    }
+    else if(tip=='not as generous'){
+      return('Thank you.')
+    }
+    else{
+      return('Bye.')
+    }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip('generous')
+switchOnCharmFromTip('not as generous')
+switchOnCharmFromTip('thanks for everything')
